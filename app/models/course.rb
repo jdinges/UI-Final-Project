a6c4ef29b7f3837b12676f6b236b7714c0702be1
Josh Dinges <jdinges@mines.edu>
2011-11-22 10:15:23 -0700

    Tried adding paper to user sign-up

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,9 @@ class Course < ActiveRecord::Base
   attr_accessible :user_id, :title, :link
   
   belongs_to :user
+  
+  def details
+    # Get brief summary of class from school website. Somehow.
+  end
+  
 end
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,24 @@ class Paper < ActiveRecord::Base
   attr_accessible :user_id, :title
   
   belongs_to :user
+  
+  def does_paper_exist
+    # Check to see if paper is exists. Check that link isn't broken
+    
+  end
+  
+  def get_info
+    # Follow link and see what extra details you can get from link.
+    
+    # Title
+    
+    # Author(s)
+    
+    # Abstract?
+  end
+  
+  def to_s
+    "#{self.title}"
+  end
+  
 end
