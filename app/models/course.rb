023e433b9a15388a21ac6505ca32d0056b6a6409
Ben Makuh <bmakuh@mines.edu>
2011-11-23 16:18:53 -0700

    Added black and white style

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,13 @@
 class Course < ActiveRecord::Base
   attr_accessible :user_id, :title, :link
   
+  validates_format_of :link, :with => URI::regexp(%w(http https))
+  
   belongs_to :user
   
   def details
     # Get brief summary of class from school website. Somehow.
+    # ... or just follow the link...
   end
   
 end
