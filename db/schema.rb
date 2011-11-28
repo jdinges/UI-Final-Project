223f7d962a9c16e99e54a3203e7c7a3497d51e97
Ben Makuh <bmakuh@mines.edu>
2011-11-28 15:54:56 -0700

    created styles model.  Run rake db:migrate AND rake db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111123193343) do
+ActiveRecord::Schema.define(:version => 20111128224210) do
 
   create_table "courses", :force => true do |t|
     t.integer  "user_id"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(:version => 20111123193343) do
   create_table "papers", :force => true do |t|
     t.integer  "user_id"
     t.string   "title",      :default => "untitled", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "styles", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
